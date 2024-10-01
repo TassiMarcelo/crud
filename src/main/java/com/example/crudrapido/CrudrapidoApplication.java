39af43908831aafa70e1a5c9a9060babc3eed8f0
Tassi <tassi.tbj@gmail.com>
2024-10-01 10:38:07 -0300

    sigue funcionando

--- src/main/java/com/example/crudrapido/CrudrapidoApplication.java
+++ src/main/java/com/example/crudrapido/CrudrapidoApplication.java
@@ -1,7 +1,10 @@
 package com.example.crudrapido;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class CrudrapidoApplication {
@@ -10,4 +13,15 @@ public class CrudrapidoApplication {
 		SpringApplication.run(CrudrapidoApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+			}
+		};
+	}
 }
